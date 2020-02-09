518a434b098e199be4dac935f0f5a9e5fac30f54
Ewa <eveinvr@gmail.com>
2020-02-09 11:37:19 +0100

    initial commit

---
+++ src/main/java/com/example/testowaniegita/TestowaniegitaApplication.java
@@ -0,0 +1,13 @@
+package com.example.testowaniegita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestowaniegitaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestowaniegitaApplication.class, args);
+    }
+
+}
